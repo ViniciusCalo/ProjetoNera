c16ba0d1e2917bae59bfa22a81b55b597db9b284
Kedsson Figueredo <kedsson19@gmail.com>
2024-06-15 22:06:56 -0300

    ih

--- backend/src/models/db.js
+++ backend/src/models/db.js
@@ -1,10 +1,14 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize('rykahwui', 'rykahwui', 'j_IehwTRhEw4ehQOJ_BAvbabJT8vVa23', {
-  dialect: 'postgres',
-  host: 'kesavan.db.elephantsql.com'
-});
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASS, {
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
+  }
+);
 
 module.exports = {
   sequelize,
