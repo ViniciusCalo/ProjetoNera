dacad2889304d68555ddd4e63cb2126ea3bcf980
thaisperlho <carvalho.thais0301@gmail.com>
2024-06-17 19:26:20 -0300

    update 1 file, delete 1 file and create 1 file

--- frontendMobile/App.jsx
+++ frontendMobile/App.jsx
@@ -3,7 +3,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import SplashScreen from './src/screens/SplashScreen';
 import HomeTeacher from './src/screens/teacher/HomeTeacher';
-import TeacherClassroom from './src/screens/TeacherClassroom';
+import TeacherClassroom from './src/screens/teacher/TeacherClassroom';
 import TeacherProfile from './src/screens/TeacherProfile';
 import LoginScreen from './src/screens/LoginScreen';
 import StudentProfile from './src/screens/student/StudentProfile';
