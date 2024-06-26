3e2635f6ad33ce852aeff3bca8a4cdbe1b5d3676
ViniciusCalo <vini.stcal@gmail.com>
2024-06-26 17:57:35 -0300

    backend: pequenas alterações nos testes

--- backend/src/__test__/userController.test.js
+++ backend/src/__test__/userController.test.js
@@ -1,52 +1,50 @@
 const request = require('supertest');
 const app = require('../app');
 const bcrypt = require('bcrypt');
-const e = require('express');
-const UserLogin = require('../models/UserModel');
+const express = require('express');
+const UserRepo = require('../repositories/UserRepository');
 
-jest.mock('../models/UserModel');
+jest.mock('../repositories/UserRepository');
 
-describe('Test the userLoginController', () => {
-    test('It should respond with a message and user object for add', async () => {
-        const user = {
+describe('Test Login and Register', () => {
+    test('Should respond with a message and a object of the user add', async () => {
+        const mockUser = {
             username: 'kedsson',
             useremail: 'testuser@gmail.com',
             userpassword: 'password',
             role: 'teacher'
         };
 
-        UserLogin.createUser.mockResolvedValue({
-            username: user.username,
-            useremail: user.useremail,
-            userpassword: await bcrypt.hash(user.userpassword, 10),
-            role: user.role
+        UserRepo.createUser.mockResolvedValue({
+            username: mockUser.username,
+            useremail: mockUser.useremail,
+            userpassword: await bcrypt.hash(mockUser.userpassword, 10),
+            role: mockUser.role
         });
 
-        const response = await request(app).post('/user/register').send(user);
+        const response = await request(app).post('/user/register').send(mockUser);
         expect(response.statusCode).toBe(201);
     });
 
     test('It should respond with a message and user object for login', async () => {
-        const user = {
+        const mockUser = {
             useremail: 'testuser@gmail.com',
             userpassword: 'password'
         };
 
-        UserLogin.loginUser.mockResolvedValue({
-            useremail: user.useremail,
-            userpassword: await bcrypt.hash(user.userpassword, 10)
+        UserRepo.loginUser.mockResolvedValue({
+            useremail: mockUser.useremail,
+            userpassword: await bcrypt.hash(mockUser.userpassword, 10)
         });
 
-        const response = await request(app).post('/user/login').send(user);
+        const response = await request(app).post('/user/login').send(mockUser);
         expect(response.statusCode).toBe(200);
         expect(response.body).toHaveProperty('message');
     });
 
     test('It should respond with a error (500) message as user not found', async () => {
         const expectedResult = { test: false };
-        UserLogin.getUserById.mockImplementation(() => {throw Error("test")});
-        //mockResolvedValue(expectedResult);
-
+        UserRepo.getUserById.mockImplementation(() => {throw Error("test")});
         const response = await request(app).get('/user/1');
         expect(response.body).toStrictEqual(expectedResult);
         expect(response.statusCode).toBe(200);
