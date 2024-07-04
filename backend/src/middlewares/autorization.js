6ef68d5e6b2ef0a3ddb9e335abdddb686a6bed36
ViniciusCalo <vini.stcal@gmail.com>
2024-07-04 19:34:12 -0300

    backend: iniciando configuração dos mock testes de autorização e autenticação

---
+++ backend/src/middlewares/autorization.js
@@ -0,0 +1,14 @@
+const passport = require('passport');
+
+const authenticateJWT = passport.authenticate('jwt', { session: false });
+
+const authorizeRole = role => {
+    return (req, res, next) => {
+        if (req.user.role !== role) {
+            return res.status(403).json({ message: "Acesso negado" });
+        }
+        next();
+    };
+};
+
+module.exports = { authenticateJWT, authorizeRole };
--- backend/src/router.js
+++ backend/src/router.js
@@ -2,11 +2,13 @@ const express = require('express');
 const passport = require('passport');
 const user = require('./controllers/UserController');
 const classroom = require('./controllers/ClassroomController');
+const { authenticateJWT, authorizeRole } = require('./middlewares/autorization');
 const router = express.Router();
 const authToken = require('./controllers/AuthenticationController');
 
+router.use(authenticateJWT);
 router.use(user.all);
-router.use(classroom.all);
+router.use(authorizeRole('professor'),classroom.all);
 router.use(authToken.verifyToken);
 
 
