d75202c13eef504344ae4af27759d8f187f09e5e
amandaSribeiro <amanda.524.ribeiro@gmail.com>
2024-07-08 21:40:30 -0300

    API: Create achievements routers

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,14 +1,15 @@
-const express = require ('express');
+const express = require('express');
 const router = require('./router');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const app = express();
 
-//Middleware-Routes
+// Middleware-Routes
 app.use(bodyParser.json());
 
-//CORS Function to API Authorization
+// CORS Function to API Authorization
 app.use(cors());
 
 app.use(router);
+
 module.exports = app;
--- backend/src/router.js
+++ backend/src/router.js
@@ -2,12 +2,14 @@ const express = require('express');
 const user = require('../src/controllers/UserController');
 const classroom = require('../src/controllers/ClassroomController');
 const classroomStudent = require('../src/controllers/ClassroomStudentController');
+const achievement = require('../src/controllers/AchievementsController');
 const router = express.Router();
 
 /*router.use() should use the router instead of call all the rout as 
 get or post on this file*/
-router.use(user.all);
-router.use(classroom.all);
-
+// router.use(user.all);
+// router.use(classroom.all);
+// router.use(classroomStudent.all);
+router.use("/api",achievement);
 
 module.exports = router;
