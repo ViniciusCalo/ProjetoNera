6b55c31f4ebedb71533c80d13a85779c81fea643
thaisperlho <carvalho.thais0301@gmail.com>
2024-07-17 17:20:41 -0300

    Update font size in ButtonBlue component

--- frontendMobile/src/components/ButtonBlue.js
+++ frontendMobile/src/components/ButtonBlue.js
@@ -29,7 +29,7 @@ const styles = StyleSheet.create({
         elevation: 5,
     },
     buttonText: {
-        fontSize: 16,
+        fontSize: 14,
         color: 'white',
         textAlign: 'center',
     },
