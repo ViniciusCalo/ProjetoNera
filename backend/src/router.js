ced3a0a41111349855b6a8621018fa5b89d90807
amandaSribeiro <amanda.524.ribeiro@gmail.com>
2024-07-17 21:38:05 -0300

    first commit

--- backend/src/router.js
+++ backend/src/router.js
@@ -3,10 +3,12 @@ const passport = require('passport');
 const user = require('./controllers/UserController');
 const classroom = require('./controllers/ClassroomController');
 const teacherController = require('./controllers/TeacherController');
+const achievement = require('../src/controllers/AchievementsController');
 const router = express.Router();
 
 router.use("/users", user);
 router.use("/classrooms", classroom);
+router.use("/api",achievement);
 
 
 module.exports = router;
