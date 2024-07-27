0aafe466743cf4d32386bf70356cefed7d3900a3
thaisperlho <carvalho.thais0301@gmail.com>
2024-07-27 20:53:43 -0300

    Updates PerfilTeacher.jsx and adds image upload functionality to the blog

--- frontendMobile/src/screens/teacher/PerfilTeacher.jsx
+++ frontendMobile/src/screens/teacher/PerfilTeacher.jsx
@@ -1,30 +1,26 @@
 import { useState } from 'react';
-import { Button, Image, View, StyleSheet, Text, Pressable } from 'react-native';
+import { Image, View, StyleSheet, Text, Pressable } from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
 import HeaderTeacher from '../../components/teacher/HeaderTeacher';
 import BottomMenuTeacher from '../../components/MenuTeacher';
 import icon1 from '../../assets/addImg.png';
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import * as FileSystem from 'expo-file-system';
+import 'react-native-get-random-values';
+import { v4 as uuidv4 } from 'uuid';
+import axios from 'axios';
+import { Buffer } from 'buffer';
+import { AZURE_STORAGE_URL, SAS_TOKEN } from '@env';
+
 
 const PerfilTeacher = () => {
-    const [image, setImage] = useState(null);
+    const [imageUri, setImageUri] = useState('');
+    const [error, setError] = useState('');
 
-    AsyncStorage.getItem('image').then((value) => {
-        setImage(value);
-    });
-
-    const storeData = async (value) => {
-        try {
-            await AsyncStorage.setItem('image', value)
-        } catch (e) {
-          // saving error
-        }
-      };
 
     const pickImage = async () => {
         // No permissions request is necessary for launching the image library
         let result = await ImagePicker.launchImageLibraryAsync({
-            mediaTypes: ImagePicker.MediaTypeOptions.All,
+            mediaTypes: ImagePicker.MediaTypeOptions.Images,
             allowsEditing: true,
             aspect: [4, 3],
             quality: 1,
@@ -32,22 +28,71 @@ const PerfilTeacher = () => {
         console.log(result);
 
         if (!result.canceled) {
-            setImage(result.assets[0].uri);
-            storeData(result.assets[0].uri);
+            const uri =  result.assets[0].uri;
+            console.log(uri);
+            const userUri =  await uploadImage(uri);
+            console.info("Setando imagem" + userUri);
+            setImageUri(userUri);
+        }
+    };
+
+    const uploadImage = async (uri) => {
+        console.log('Uploading image');
+        const imageUri = uri;
+
+        // Substitua pela sua URL base da conta de armazenamento e o token SAS
+        const baseUrl = AZURE_STORAGE_URL;
+        const sasToken = SAS_TOKEN;
+
+        
+        try {
+            // const fileInfo = await FileSystem.getInfoAsync(imageUri);
+            const extension = imageUri.split('.').pop();
+            const mimeType = "image/" + extension; 
+
+            // Gera um novo nome para o arquivo
+            const fileId = uuidv4() || 'invalid-file-id';
+            const newFileName = fileId + '.' + extension;
+            console.log("info: " + newFileName);
+            const signedUrl = `${baseUrl}/${newFileName}?${sasToken}`;
+
+
+            // const fileStats = await RNFS.stat(imageUri);
+            const fileContent = await FileSystem.readAsStringAsync(imageUri, {
+                encoding: FileSystem.EncodingType.Base64,
+              });
+            console.log("info" + fileContent);
+
+            const options = {
+                headers: {
+                    'Content-Type': mimeType,
+                    'x-ms-blob-type': 'BlockBlob',
+                },
+            };
+
+            const response = await axios.put(signedUrl, Buffer.from(fileContent, 'base64'), options);
+
+            if (response.status === 201) {
+                console.log('Success Image uploaded successfully');
+            } else {
+                console.log('Error Image upload failed');
+            }
+            return signedUrl;
+        } catch (err) {
+            console.log('Error Unknown error: ' + err.message);
+            setError(JSON.stringify(err));
         }
     };
 
     return (
         <View style={styles.container}>
             <View style={styles.header}>
-            <HeaderTeacher
-            image={image}
-            />
+                <HeaderTeacher />
             </View>
             <Text style={styles.title}>Perfil</Text>
             <View style={styles.infoPerfil}>
                 <View style={styles.image}>
-                    {image && <Image source={{ uri: image }} style={styles.img} />}
+                    {imageUri && <Image source={{ uri: imageUri }} style={styles.img} />}
                     <Pressable onPress={pickImage} style={styles.btnfoto}>
                         <Image source={icon1} style={styles.icon} />
                     </Pressable>
@@ -56,6 +101,7 @@ const PerfilTeacher = () => {
             </View>
             <View style={styles.infoText}>
                 <Text style={styles.texto}>Notificações</Text>
+                <Text style={styles.texto}>{error}</Text>
 
             </View>
 
