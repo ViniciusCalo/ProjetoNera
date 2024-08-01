663504fbf6fcdb7091f2ddaeae32013313d0193c
amandaSribeiro <amanda.524.ribeiro@gmail.com>
2024-07-31 22:16:52 -0300

    Controller: ajustes no StudentController

--- backend/src/controllers/StudentController.js
+++ backend/src/controllers/StudentController.js
@@ -2,7 +2,6 @@ const studentRepo = require('../repositories/StudentRepository');
 const passport = require('passport');
 const express = require('express');
 const router = express.Router();
-
 const registerUserStudent = async ({ userid }) => {
     try {
         const newStudent = await studentRepo.registerUserAsAStudent({
@@ -10,58 +9,57 @@ const registerUserStudent = async ({ userid }) => {
         });
         return newStudent;
     } catch (error) {
-        throw new Error('Erro ao criar aluno: ', error.message);
+        throw new Error('Erro ao criar aluno: ' + error.message);
     }
-}
+};
 
-const googleRegisterUserAsStudent = async ({userid }) => {
-    try{
+const googleRegisterUserAsStudent = async ({ userid }) => {
+    try {
         const newStudent = await studentRepo.registerUserAsAStudentViaGoogle({
             userid: userid
         });
         return newStudent;
     } catch (error) {
-        throw new Error('Erro ao registrar aluno via Google: ', error.message);
+        throw new Error('Erro ao registrar aluno via Google: ' + error.message);
     }
-}
+};
 
 const studentLogin = async ({ useremail }) => {
     try {
         const { token, name, profilePic } = await studentRepo.loginStudent({ useremail });
         return { token, name, profilePic };
     } catch (error) {
-        throw new Error('Erro no login do aluno: ', error.message);
+        throw new Error('Erro no login do aluno: ' + error.message);
     }
-}
+};
 
-const studentGoogleLogin = async ({useremail}) => {
+const studentGoogleLogin = async ({ useremail }) => {
     try {
         const { token, name, profilePic } = await studentRepo.loginStudentGoogle({ useremail });
         return { token, name, profilePic };
     } catch (error) {
         throw new Error('Erro no login do aluno: ' + error.message);
     }
-}
+};
 
 const getStudentById = async (request, response) => {
     try {
         console.log("Student: ", request.user);
         const student = await studentRepo.getStudentById(request);
         if (!student) {
-            return response.status(404).json({ message: error.message || "Student not found" });
+            return response.status(404).json({ message: "Student not found" });
         }
         return response.status(200).json(student);
-
     } catch (error) {
         console.error(error);
         return response.status(500).json({ message: error.message || "Internal server error" });
     }
-}
+};
 
 module.exports = {
     registerUserStudent,
-    studentLogin,
-    getStudentById,
     googleRegisterUserAsStudent,
-    studentGoogleLogin
- };
+    studentLogin,
+    studentGoogleLogin,
+    getStudentById
+};
