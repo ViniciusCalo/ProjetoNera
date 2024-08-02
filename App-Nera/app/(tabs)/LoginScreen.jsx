e5014259309a5e7f678f649dcf7effa5286abde3
thaisperlho <carvalho.thais0301@gmail.com>
2024-08-02 12:34:46 -0300

    update LoginScreen.jsx

--- App-Nera/app/(tabs)/LoginScreen.jsx
+++ App-Nera/app/(tabs)/LoginScreen.jsx
@@ -185,7 +185,7 @@ const LoginScreen = ({ navigationA }) => {
                             <Text style={stylesForm.textButton}>Entrar</Text>
                         </Pressable>
 
-                        <Pressable style={stylesForm.button_google} /* onPress={() => promptAsync()} */ onPress={() => navigation.navigate('HomeTeacher')}>
+                        <Pressable style={stylesForm.button_google} /* onPress={() => promptAsync()} */ onPress={() => navigation.navigate('StudentProfile')}>
 
                             <Text style={[stylesForm.textButton, { color: '#3F3F3F' }]}>Google</Text>
                         </Pressable>
