5573af30c59b7d05f31d0fd8573d2ab0f728a0d3
thaisperlho <carvalho.thais0301@gmail.com>
2024-09-18 20:03:27 -0300

    update styles.js menuLateralProf

--- frontendWeb/src/components/MenuLateralProf/styles.js
+++ frontendWeb/src/components/MenuLateralProf/styles.js
@@ -5,29 +5,24 @@ export const Menu = styled.nav`
     height: 100%;
     background: #ffffff;
     border-radius: 0 20px 20px 0;
-    width: 2%;
-    padding: 2%;
+    width: 5%;
     box-shadow: 4px 4px 5px rgba(0, 0, 0, 0.25);
     overflow-x: hidden;
     transition: 0.5s;
     z-index: 1;
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-    align-items: center;
-
     &:hover {
-        width: 15%;
+        width: 20%;
     }
     `;
 
 export const Lista = styled.ul`
     position: absolute;
-    top: 0;
-    left: 0;
+    top: 5%;
     width: 100%;
-    padding-left: 5px;
-    padding-top: 5px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-between;
 `;
 
 export const Item = styled.li`
@@ -35,23 +30,23 @@ export const Item = styled.li`
     width: 100%;
     margin-bottom: 15px;
     margin-left: 20px;
-    border-top-left-radius: 20px;
-    border-bottom-left-radius: 20px;
 
     &:hover {
-        width: 80%;
-    border-radius: 20px;
-    background: rgba(3, 76, 140, 0.24);
+        padding: 5%;
+        margin-left: 10px;
+        border-top-right-radius: 20px;
+        border-bottom-right-radius: 20px;
+        background: rgba(3, 76, 140, 0.24);
     }
     `;
 export const Item2 = styled.li`
-margin-left: 50%;
+    margin-left: 50%;
     list-style: none;
     width: 100%;
     margin-top: 60px;
     margin-bottom: 60px;
     `;
-    
+
 
 export const Link = styled.a`
     display: flex;
@@ -71,10 +66,10 @@ export const Icone2 = styled.img`
 `;
 export const Texto = styled.p`
     color: #000;
-font-family: Inter;
-font-size: 20px;
-font-style: normal;
-font-weight: 400;
-line-height: normal;
-text-transform: capitalize;
+    font-family: Inter;
+    font-size: 20px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: normal;
+    text-transform: capitalize;
 `;
