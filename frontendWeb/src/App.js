ed39417d6fcb8b6bf6a2727e811775fce6940b91
Kedsson Figueredo <kedsson19@gmail.com>
2024-11-03 17:59:51 -0300

    build(deps): update package-lock.json, package.json, App.js and frontendWeb/src/components/TelaLoginAluno/index.js

--- frontendWeb/src/App.js
+++ frontendWeb/src/App.js
@@ -1,40 +1,43 @@
 import React from 'react';
-//import GlobalStyle from './styles/global'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
-import Perfil from './pages/Perfil'
+import Perfil from './pages/Perfil';
 import PerfilProf from './pages/PerfilProf';
-import Trilha from './pages/Trilha'
-import Modulo from './pages/Modulo'
-import Questao from './pages/Questao'
+import Trilha from './pages/Trilha';
+import Modulo from './pages/Modulo';
+import Questao from './pages/Questao';
 import Questao2 from './pages/Questao2';
 import LoginAluno from './pages/LoginAluno';
 import LoginProf from './pages/LoginProf';
 import TeacherClass from './pages/TeacherClass';
 import CreateClass from './pages/CreateClass';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-
 import Cadastro from './pages/Cadastro';
+
+// Substitua pelo seu Client ID da API Google
+const GOOGLE_CLIENT_ID = "925583381049-fgqie9ocvvaojvpvg0acvkisgnl9erst.apps.googleusercontent.com";
 
 function App() {
   return (
-    <BrowserRouter>
-    <Routes>
-      <Route>
-        <Route path="perfil" element={<Perfil />} /> 
-        <Route path="perfilProf" element={<PerfilProf />} />   
-        <Route path="trilha" element={<Trilha />} />
-        <Route path="modulo" element={<Modulo />} />
-        <Route path="questao" element={<Questao />} />
-        <Route path="questao2" element={<Questao2 />} />
-        <Route path="loginAluno" element={<LoginAluno/>}/>
-        <Route path="loginProfessor" element={<LoginProf/>}/>
-        <Route path="cadastro" element={<Cadastro/>}/>
-        <Route path="teacherClass" element={<TeacherClass/>}/>
-        <Route path='createClass' element={<CreateClass/>}/>
-      </Route>
-    </Routes>
-  </BrowserRouter>
-  )
+    <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LoginAluno />} /> {/* Página inicial */}
+          <Route path="perfil" element={<Perfil />} /> 
+          <Route path="perfilProf" element={<PerfilProf />} />   
+          <Route path="trilha" element={<Trilha />} />
+          <Route path="modulo" element={<Modulo />} />
+          <Route path="questao" element={<Questao />} />
+          <Route path="questao2" element={<Questao2 />} />
+          <Route path="loginAluno" element={<LoginAluno />} />
+          <Route path="loginProfessor" element={<LoginProf />} />
+          <Route path="cadastro" element={<Cadastro />} />
+          <Route path="teacherClass" element={<TeacherClass />} />
+          <Route path="createClass" element={<CreateClass />} />
+        </Routes>
+      </BrowserRouter>
+    </GoogleOAuthProvider>
+  );
 }
 
 export default App;
--- frontendWeb/src/components/TelaLoginAluno/index.js
+++ frontendWeb/src/components/TelaLoginAluno/index.js
@@ -1,71 +1,111 @@
-import React from 'react'
-import * as C from './styles'
-import Logo from './img/logo.png'
-import google from './img/google.svg'
-import { useState } from "react";
-import axios from "axios";
+import React, { useState } from 'react';
+import * as C from './styles';
+import Logo from './img/logo.png';
+import google from './img/google.svg';
+import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
-
-
+import { useGoogleLogin } from '@react-oauth/google'; // Correção para a importação do Google Login
 
 const TelaLoginAluno = () => {
-  const nav = useNavigate()
-  const [sessao, setSesao] = useState([])
-  const [email, setEmail] = useState('')
-  const [senha, setSenha] = useState('')
+  const nav = useNavigate();
+  const [sessao, setSessao] = useState([]);
+  const [email, setEmail] = useState('');
+  const [senha, setSenha] = useState('');
 
+  const handleEmail = (e) => {
+    setEmail(e.target.value);
+  };
 
-const handleEmail = (e) => {
-  setEmail(e.target.value)
-}
+  const handleSenha = (e) => {
+    setSenha(e.target.value);
+  };
 
-const handleSenha = (e) => {
-  setSenha(e.target.value)
-}
+  const handleVerificarUsuario = async () => {
+    const body = {
+      email: email,
+      senha: senha
+    };
 
+    try {
+      const uri = process.env.REACT_APP_API_URL || "http://localhost:3001";
+      const response = await axios.post(`${uri}/usuario/login`, body);
+      setSessao(response.data.message);
+      console.log(response.data.message);
+      localStorage.setItem('usuario', email);
+      nav('/perfil');
+    } catch (err) {
+      console.log(err);
+      alert("Senha e/ou email incorreto");
+    }
+  };
 
-const handleVerificarUsuario = async () => {
-  var body = {
-    "email": email,
-    "senha": senha
-}
-try {
-    const uri = process.env.REACT_APP_API_URL || "http://localhost:3001";
-    const response = await axios.post(`${uri}/usuario/login`, body)
-    setSesao(response.data.message);
-    console.log(sessao)
-    localStorage.setItem('usuario', email);
-    nav('/perfil')
-} catch (err) {
-    console.log(err);
-    alert("senha e/ou email incorreto")
-}
-}
+  const [userInfo, setUserInfo] = useState(null);
 
+  const loginSocial = useGoogleLogin({
+    onSuccess: async (tokenResponse) => {
+      try {
+        // Chamada à API do Google para obter os dados do usuário
+        const userResponse = await axios.get(
+          'https://www.googleapis.com/oauth2/v3/userinfo',
+          {
+            headers: {
+              Authorization: `Bearer ${tokenResponse.access_token}`,
+            },
+          }
+        );
+        
+        // Defina as informações do usuário no estado
+        setUserInfo(userResponse.data);
+        console.log("Dados do usuário:", userResponse.data);
+      } catch (error) {
+        console.error("Erro ao buscar dados do usuário:", error);
+      }
+    },
+    onError: (error) => {
+      console.error("Erro durante o login:", error);
+      alert('Erro durante o login com Google');
+    },
+  });
+  
   return (
     <C.Box>
-        <C.Logo src={Logo}/>
-        <C.Container>
+      <C.Logo src={Logo} alt="Logo" />
+      <C.Container>
         <C.DivButton>
-        <C.ButtonAlu>Estudante</C.ButtonAlu>
+          <C.ButtonAlu>Estudante</C.ButtonAlu>
           <C.ButtonProf href='/loginProfessor'>Professor</C.ButtonProf>
         </C.DivButton>
-        <C.FormLogin autocomplete="off">
-        <C.InputE id='email' onChange={handleEmail} type="text" placeholder="E-mail ou nome do usuário"/>
-        <C.InputS id='senha' onChange={handleSenha} type="password" placeholder="Senha"/>
-        <C.div>
-            <C.Link>Esqueci minha senha?</C.Link>
-        </C.div>
-        <C.Button type='button' onClick={handleVerificarUsuario}>Entrar</C.Button>
-        <C.DivLinha>
+        <C.FormLogin autoComplete="off">
+          <C.InputE
+            id='email'
+            onChange={handleEmail}
+            type="text"
+            placeholder="E-mail ou nome do usuário"
+          />
+          <C.InputS
+            id='senha'
+            onChange={handleSenha}
+            type="password"
+            placeholder="Senha"
+          />
+          <C.div>
+            <C.Link href="#">Esqueci minha senha?</C.Link>
+          </C.div>
+          <C.Button type='button' onClick={handleVerificarUsuario}>
+            Entrar
+          </C.Button>
+          <C.DivLinha>
             <C.linha1></C.linha1>
             ou
             <C.linha2></C.linha2>
-        </C.DivLinha>
-        <C.ButtonG><C.icon src={google} />Login com Google</C.ButtonG>
-        <C.ButtonC href='/cadastro' >Criar Conta</C.ButtonC>
+          </C.DivLinha>
+          <C.ButtonG type='button' onClick={() => loginSocial()}>
+            <C.icon src={google} alt="Google logo" />
+            Login com Google
+          </C.ButtonG>
+          <C.ButtonC href='/cadastro'>Criar Conta</C.ButtonC>
         </C.FormLogin>
-        </C.Container>
+      </C.Container>
     </C.Box>
   );
 };
