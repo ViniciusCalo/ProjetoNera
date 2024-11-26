46c81127bbb1629fb11a5af42dce7f3291204a9b
Kedsson Figueredo <kedsson19@gmail.com>
2024-11-26 20:31:20 -0300

    chore: delete spec.cy.js

--- frontendWeb/cypress/e2e/specStudent.cy.js
+++ frontendWeb/cypress/e2e/specStudent.cy.js
@@ -1,10 +1,17 @@
-describe('template spec', () => {
+describe('Passo a Passo do login do aluno até a entrada do primeiro jogo', () => {
   it('passes', () => {
+    //Abrindo a tela inicial
     cy.visit('http://localhost:3000/cadastro')
+
+    //Selecionando login como aluno
     cy.get('a').contains('Entrar').click()
+
+    //Preenchendo os dados necessarios para login e realizando o login
     cy.get('input[placeholder="E-mail ou nome do usuário"]').type('lucas@email.com')
     cy.get('input[placeholder="Senha"]').type('1234')
     cy.get('button').contains('Entrar').click()
+
+    //acessando o primeiro jogo no modulo de fração
     cy.get('a[href="/trilha"]').click()
     cy.get('a[href="/modulo"]').contains('Trilha de Frações').click()
     cy.get('img[alt="Modulo 1"]').click()
