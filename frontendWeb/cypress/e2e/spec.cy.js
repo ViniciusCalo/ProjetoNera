cc212d32689259a8627ba36cc823af9c9ba6ec16
Kedsson Figueredo <kedsson19@gmail.com>
2024-11-26 19:23:05 -0300

    update spec.cy.js

--- frontendWeb/cypress/e2e/spec.cy.js
+++ frontendWeb/cypress/e2e/spec.cy.js
@@ -5,5 +5,8 @@ describe('template spec', () => {
     cy.get('input[placeholder="E-mail ou nome do usuário"]').type('lucas@email.com')
     cy.get('input[placeholder="Senha"]').type('1234')
     cy.get('button').contains('Entrar').click()
+    cy.get('a[href="/trilha"]').click()
+    cy.get('a[href="/modulo"]').contains('Trilha de Frações').click()
+    cy.get('img[alt="Modulo 1"]').click()
   })
 })
