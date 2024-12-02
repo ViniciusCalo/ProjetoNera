d82b3ce02afbcb3d15b09a8cd44ee35498d418f9
ViniciusCalo <vini.stcal@gmail.com>
2024-12-02 20:03:16 -0300

    ajustando diretórios

--- frontendMobile/src/app/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit'
-import userRedducer from '../features/user/userSlice'
-
-export default configureStore({
-  reducer: {
-    user: userRedducer,
-  },
-})
-
