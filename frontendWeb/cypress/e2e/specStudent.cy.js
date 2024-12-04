8cfa62d476942cb114a245b13a1010785333033a
thaisperlho <carvalho.thais0301@gmail.com>
2024-12-03 22:35:00 -0300

    update specStudent.cy.js and specTeacher.cy.js

--- frontendWeb/cypress/e2e/specStudent.cy.js
+++ frontendWeb/cypress/e2e/specStudent.cy.js
@@ -1,7 +1,7 @@
 describe('Passo a Passo do login do aluno até a entrada do primeiro jogo', () => {
   it('passes', () => {
     //Abrindo a tela inicial
-    cy.visit('http://localhost:3000/cadastro')
+    cy.visit('http://localhost:3000/register')
 
     //Selecionando login como aluno
     cy.get('a').contains('Entrar').click()
@@ -12,8 +12,8 @@ describe('Passo a Passo do login do aluno até a entrada do primeiro jogo', () =
     cy.get('button').contains('Entrar').click()
 
     //acessando o primeiro jogo no modulo de fração
-    cy.get('a[href="/trilha"]').click()
-    cy.get('a[href="/modulo"]').contains('Trilha de Frações').click()
+    cy.get('a[href="/track"]').click()
+    cy.get('a[href="/module"]').contains('Trilha de Frações').click()
     cy.get('img[alt="Modulo 1"]').click()
   })
 })
